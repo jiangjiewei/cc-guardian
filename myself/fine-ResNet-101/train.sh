dcb66cceea4dc9520a699e966e9fd5ce5d86b8aa
longerping <jiangjw924@126.com>
2017-04-19 16:37:12 +0800

    first

---
+++ myself/fine-ResNet-101/train.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+TOOLS=./build/tools
+
+GLOG_logtostderr=1 $TOOLS/caffe train --solver=./myself/fine-ResNet-101/resnet_101_solver.prototxt --weights ./myself/fine-ResNet-101/ResNet-101-model.caffemodel -gpu 0,1
+
+echo "Done."
+
+
+
+
+
+
+
+
